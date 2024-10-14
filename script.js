ea44424a282bb701769b9be42c7851d63667146f
omlan99 <omlan10616@gmail.com>
2024-10-14 11:57:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const getData = () => {
+    // let productName = document.getElementById('product-Name');
+    // let productQuantity = document.getElementById('product-Quantity');
+    // const product = productName.value;
+    // const quantity = productQuantity.value;
+    // addItem(product, quantity);
+    // productName.value = '';
+    // productQuantity.value = '';
+}
+
+const addItem = (product, quantity) => {
+    // let liContainer = document.querySelector('.list-container');
+    // const listItem = document.createElement('li');
+    // listItem.innerText = `${product} : ${quantity}`
+    // liContainer.appendChild(listItem)
+
+
+}
+const checkAvailability = () => {
+    // let device = {};
+
+    // if(localCard){
+    //     device = JSON.parse(localStore)
+    // } 
+}
+const localStorage = (product,quantity) => {
+
+}
+localStorage.setItem('hello', 'world')
+
+const hello = localStorage.getItem("hello");
+console.log(hello)
